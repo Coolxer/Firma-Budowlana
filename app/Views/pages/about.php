3ad8c17c30624d989aaf8c72ee32199f3329807f
coolxer <lukasik4789@gmail.com>
2020-11-25 21:20:17 +0100

    fix home page

---
+++ app/Views/pages/about.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="assets/css/pages/about.css">
+
+<div class="content col-10">
+    <div class="row main-box">
+        <div class="col-xl-3 col-sm-12 icons">
+            <div class="row">
+                <i class="far fa-building col-xl-12 col-6"></i>
+                <i class="fas fa-hammer fa-flip-horizontal col-xl-12 col-6"></i>
+            </div>
+        </div>
+        <div class="jumbotron col-xl-8 col-sm-12">
+            <h1 class="display-4 jumb-header">Firma Budowlana</h1>
+            <hr class="my-4">
+            <p class="lead"><i class="fas fa-check"></i>Realizujemy usługi najwyższej jakości. Z nami zbudujesz swój dom i wyremontujesz swoje mieszkanie!</p>
+            <p class="lead"><i class="fas fa-check"></i>Zaprojektujemy twój wymarzony dom lub wykonamy gotowy projekt!</p>
+            <p class="lead"><i class="fas fa-check"></i>Zapewniamy materiały najwyższej jakości w konkurencyjnej cenie!</p>
+            <hr class="my-4">
+            <p class="lead dont-wait">Nie czekaj! Już teraz złóż zgłoszenie, a otrzymasz natychmiastową ofertę!</p>
+            <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Złóż zgłoszenie</a>
+        </div>
+    </div>
+
+</div>
---
+++ app/Views/pages/contact.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="assets/css/pages/contact.css">
+
+<div class="contact-box row">
+    <div class="col">
+        <p class="questions">
+            Jeśli masz jakieś pytania, uwagi lub chcesz zawrzeć z nami współpracę, prosimy o kontakt
+        </p>
+
+
+    </div>
+</div>
